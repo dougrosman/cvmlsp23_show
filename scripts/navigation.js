bbecfd4d7a2a704bc9c07da2b553425d90129c65
Doug Rosman <dougrosman@gmail.com>
2023-07-13 03:29:20 -0500

    gettin there

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -20,14 +20,43 @@ let sketch = function (p) {
     if (detections != undefined) {
       if (detections.multiHandLandmarks != undefined) {
         p.drawHands();
-        p.drawFingerTips();
+        if (detections.multiHandLandmarks.length == 2) {
+          p.drawFingerTips();
+        }
       }
     }
   };
 
-  p.drawFingerTips = function() {
-    
-  }
+  p.drawFingerTips = function () {
+    const INDEX_TIP_1 = detections.multiHandLandmarks[0][8];
+    const INDEX_TIP_2 = detections.multiHandLandmarks[1][8];
+
+    const INDEX1 = p.createVector(
+      p.width - INDEX_TIP_1.x * p.width,
+      INDEX_TIP_1.y * p.height,
+      INDEX_TIP_1.z
+    );
+
+    const INDEX2 = p.createVector(
+      p.width - INDEX_TIP_2.x * p.width,
+      INDEX_TIP_2.y * p.height,
+      INDEX_TIP_2.z
+    );
+
+    const INDEX_TIP_DISTANCE = INDEX1.dist(INDEX2)
+
+    const INDEX_TIP_MIDPOINT = INDEX1.lerp(INDEX2, 0.5);
+
+    if (INDEX_TIP_DISTANCE < 15) {
+        p.noFill();
+        p.stroke(0, 255, 0);
+        p.ellipse(INDEX_TIP_MIDPOINT.x, INDEX_TIP_MIDPOINT.y, 20);
+        
+        // toggleWallText();
+      } else {
+        // toggleWallText();
+      }
+  };
 
   p.drawHands = function () {
     p.strokeWeight(4);
@@ -49,19 +78,14 @@ let sketch = function (p) {
         INDEX_FINGER_TIP.z
       );
 
-      
       const FINGER_DISTANCE = THUMB.dist(INDEX);
 
       const selectPointVector = THUMB.lerp(INDEX, 0.5);
-      
+
       if (FINGER_DISTANCE < 15) {
-          p.noFill();
-          p.stroke(0, 255, 0);
-          p.ellipse(
-            selectPointVector.x,
-            selectPointVector.y,
-            clickedCounter / 4
-          );
+        p.noFill();
+        p.stroke(0, 255, 0);
+        p.ellipse(selectPointVector.x, selectPointVector.y, clickedCounter / 4);
         clickedCounter += 4;
 
         if (clickedCounter > 300) {
@@ -83,3 +107,31 @@ let sketch = function (p) {
 let myp5 = new p5(sketch);
 
 
+
+// show or hide wall text
+let wallTextVisible = false;
+
+// $(window).click(function () {
+//   if (wallTextVisible) {
+//     showArtwork();
+//   } else {
+//     showWallText();
+//   }
+// });
+
+
+function showWallText() {
+    $(".artwork").addClass("blur-in");
+    $(".wall-text").addClass("fade-in");
+    $(".artwork").removeClass("blur-out");
+    $(".wall-text").removeClass("fade-out");
+    wallTextVisible = !wallTextVisible;
+}
+
+function hideWallText() {
+    $(".artwork").addClass("blur-out");
+    $(".wall-text").addClass("fade-out");
+    $(".artwork").removeClass("blur-in");
+    $(".wall-text").removeClass("fade-in");
+    wallTextVisible = !wallTextVisible;
+}
